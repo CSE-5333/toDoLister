218038a5aaf361d1edf1b49dc77e2baffa456d62
Bivash Yadav <byadav02717@gmail.com>
2021-11-22 17:01:50 -0600

    implemented login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,43 @@
-import { Avatar, Button, Grid, Paper, TextField } from "@mui/material";
+import { Avatar, Button, Grid, Paper, TextField, Zoom } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import { blue } from "@mui/material/colors";
 import { useHistory } from 'react-router';
 import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 function Login() {
+    const {login} = useAuth();
+    const [Password, setPassword] = useState("")
+    const [Email, setEmail] = useState("")
+    const [isLoading, setisLoading] = useState(false)
     let history = useHistory();
-    const goTo = () =>{
-        history.push('/home')
+    const notify = () =>{
+        toast("Wow so easy!",{
+            className:"custom-toast",
+            draggable: true,
+            position: toast.POSITION.BOTTOM_CENTER
+        });
+    }
+    async function handleSubmit(e){
+        //history.push('/home')
+        setisLoading(true);
+        console.log(Email)
+        console.log(Password)
+        login(Email, Password)
+        .then((response)=>{
+            console.log(response)
+            history.push('/home')
+        })
+        .catch((error)=>{
+            console.log(error)
+            setisLoading(false)
+            
+        });
     }
     const paperStyle = {
         padding: 20,
@@ -18,6 +47,7 @@ function Login() {
     };
     return (
         <div>
+            <ToastContainer draggable={false} transition={Zoom} autoClose={8000}/>
             <div style={{ backgroundColor: "#9CC3D5FF", padding: "15px", height: "100vh"}}>
             <Grid>
                 <Paper elevation={10} style={paperStyle}>
@@ -32,6 +62,9 @@ function Login() {
                         label='Email'
                         placeholder='Enter your email'
                         fullWidth
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                     />
 
                     <TextField
@@ -40,10 +73,13 @@ function Login() {
                         type='password'
                         fullWidth
                         margin="normal"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
 
                     />
 
-                    <Button sx={{mt:3}} type='submit' onClick={goTo} color='primary' variant='contained' fullWidth>
+                    <Button sx={{mt:3}} type='submit' onClick={handleSubmit} color='primary' variant='contained' fullWidth disabled={isLoading}>
                         Login
                     </Button>
 
